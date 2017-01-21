823191baae8dfb3e8b28e8756f8488be8c84fb99
Kenta Iwasaki <dranithix@gmail.com>
2017-01-21 14:24:36 -0500

    Working on question layout

--- client/home/home.js
+++ client/home/home.js
@@ -2,17 +2,17 @@
  * Created by Kenta Iwasaki on 1/21/2017.
  */
 import annotator from "annotator";
-import Chart from "chartjs";
+import Chart from "chart.js";
 
 Template.home.viewmodel({
     questions: [],
     onCreated() {
         Meteor.subscribe('exams.all');
 
-        let app = new annotator.App();
-        app.include(annotator.ui.main);
-        app.include(annotator.storage.http);
-        app.start();
+        // let app = new annotator.App();
+        // app.include(annotator.ui.main);
+        // app.include(annotator.storage.http);
+        // app.start();
     },
 
     onRendered() {
@@ -21,12 +21,12 @@ Template.home.viewmodel({
             {
                 type: 'pie',
                 data: {
-                    labels: ["Progress"],
+                    labels: ["Progress", "Todo"],
                     datasets: [
 
                         {
                             label: "Percentage",
-                            data: [100],
+                            data: [65, 35],
                             backgroundColor: ["#FF6384"],
                             hoverBackgroundColor: ["#FF6384"]
                         }
@@ -34,41 +34,17 @@ Template.home.viewmodel({
                 },
 
             }
-        )
-    },
+)
+},
 
-    autorun() {
-        const exams = Exams.find({}).fetch();
+autorun() {
+    const exams = Exams.find({}).fetch();
 
-        let questions = [];
-        _.each(exams, exam => {
-            let firstCount = exam.text.match(/ [A-Z\d]\. /g);
-            let secondCount = exam.text.match(/ \([A-Z\d]\) /g);
-            firstCount = firstCount && firstCount.length || 0;
-            secondCount = secondCount && secondCount.length || 0;
-
-            if (firstCount > secondCount) {
-                exam = exam.text.match(/\b(\d{1,2})\.\s\D*(?:(?!\b\d{1,3}\.\s)\d+\D*)*/g);
-                exam = _.filter(exam, exam => !/[@]*(SAMPLE)/g.test(exam) && exam.length > 150)
-
-                _.each(exam, exam => {
-                    let question = exam.replace(/\d+[(.]\s*/g, '');
-
-                    let choices = [];
-                    if (question.match(/\b[A-Z][.)].*/g)) choices = _.map(_.filter(question.match(/\b[A-Z][.)].*/g)[0].split(/[A-Z\d][.)] /g), text => text !== ""), text => text.trim());
-                    choices = _.filter(choices, choice => choice.length < 50);
-
-                    if (choices.length > 1 && choices.length < 5) {
-                        questions.push({
-                            question: question.replace(/\b[A-Z][.)].*/g, ''),
-                            choices
-                        })
-                    }
-                });
-            }
-        })
-
-        console.log(questions)
-        this.questions(questions);
-    }
+    let questions = [];
+    _.each(exams, exam => questions = questions.concat(_.map(exam.questions, question => {
+        question.url = exam.url;
+        return question;
+    })));
+    this.questions(questions);
+}
 })
---
+++ server/methods.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Kenta Iwasaki on 1/21/2017.
+ */
+
+Meteor.methods({
+    'subjects': function() {}
+
+})
