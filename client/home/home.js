9b75d76c332fb8ba8cb892cfbf9d029c7acd5944
Kenta Iwasaki <dranithix@gmail.com>
2017-01-21 13:34:31 -0500

    Fixed null error and checked count for skipping a few exam docs.

--- client/home/home.js
+++ client/home/home.js
@@ -39,13 +39,15 @@ Template.home.viewmodel({
 
     autorun() {
         const exams = Exams.find({}).fetch();
-        console.log(exams);
 
         let questions = [];
         _.each(exams, exam => {
-            const firstCount = exam.text.match(/ [A-Z\d]\. /g).length;
-            const secondCount = exam.text.match(/ \([A-Z\d]\) /g).length;
-            if (firstCount < secondCount) {
+            let firstCount = exam.text.match(/ [A-Z\d]\. /g);
+            let secondCount = exam.text.match(/ \([A-Z\d]\) /g);
+            firstCount = firstCount && firstCount.length || 0;
+            secondCount = secondCount && secondCount.length || 0;
+
+            if (firstCount > secondCount) {
                 exam = exam.text.match(/\b(\d{1,2})\.\s\D*(?:(?!\b\d{1,3}\.\s)\d+\D*)*/g);
                 exam = _.filter(exam, exam => !/[@]*(SAMPLE)/g.test(exam) && exam.length > 150)
 
@@ -53,12 +55,15 @@ Template.home.viewmodel({
                     let question = exam.replace(/\d+[(.]\s*/g, '');
 
                     let choices = [];
-                    if (question.match()) choices = _.map(_.filter(question.match(/\b[A-Z][.)].*/g)[0].split(/[A-Z\d][.)] /g), text => text !== ""), text => text.trim());
-                    choices = _.filter(choices, choice => choice.length < 50)
-                    if (choices.length > 1 && choices.length < 5) questions.push({
-                        question: question.replace(/\b[A-Z][.)].*/g, ''),
-                        choices
-                    })
+                    if (question.match(/\b[A-Z][.)].*/g)) choices = _.map(_.filter(question.match(/\b[A-Z][.)].*/g)[0].split(/[A-Z\d][.)] /g), text => text !== ""), text => text.trim());
+                    choices = _.filter(choices, choice => choice.length < 50);
+
+                    if (choices.length > 1 && choices.length < 5) {
+                        questions.push({
+                            question: question.replace(/\b[A-Z][.)].*/g, ''),
+                            choices
+                        })
+                    }
                 });
             }
         })
