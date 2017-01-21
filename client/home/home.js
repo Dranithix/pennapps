c44dfc6a01081d1a19c7f73667c5c0a19710c295
helfi92 <helfi92@gmail.com>
2017-01-21 16:42:17 -0500

    Fix Vote up

--- client/home/home.js
+++ client/home/home.js
@@ -4,87 +4,89 @@
 import Chart from "chart.js";
 
 Template.home.viewmodel({
-    questions: [],
-    onCreated() {
-        Meteor.subscribe('exams.all');
+  questions: [],
+  onCreated() {
+    Meteor.subscribe('exams.all');
 
-        // let app = new annotator.App();
-        // app.include(annotator.ui.main);
-        // app.include(annotator.storage.http);
-        // app.start();
-    },
+    // let app = new annotator.App();
+    // app.include(annotator.ui.main);
+    // app.include(annotator.storage.http);
+    // app.start();
+  },
 
-    onRendered() {
-        const progressChart = new Chart(
-            $("#progressChart"),
+  onRendered() {
+    const progressChart = new Chart(
+      $("#progressChart"),
+      {
+        type: 'pie',
+        data: {
+          labels: ["Progress", "Todo"],
+          datasets: [
             {
-                type: 'pie',
-                data: {
-                    labels: ["Progress", "Todo"],
-                    datasets: [
-                        {
-                            label: "Percentage",
-                            data: [65, 35],
-                            backgroundColor: ["#FF6384"],
-                            hoverBackgroundColor: ["#FF6384"]
-                        }
-                    ]
-                },
-
+              label: "Percentage",
+              data: [65, 35],
+              backgroundColor: ["#FF6384"],
+              hoverBackgroundColor: ["#FF6384"]
             }
-        )
-    },
+          ]
+        },
 
-    autorun() {
-        if (this.parent().subject()) {
-            const exams = Exams.find({category: this.parent().subject()}).fetch();
+      }
+    )
+  },
 
-            if (exams.length) {
-                let questions = [];
-                _.each(exams, exam => questions = questions.concat(_.map(exam.questions, (question, index) => {
-                    question.rawQuestion = question.question;
-                    question.question = (index + 1) + ". " + question.question;
-                    question.url = exam.url;
-                    question.choices = _.map(question.choices, (choice, index) => choice = "("+ (index + 1) + ") " + choice);
-                    question.exam = exam._id;
+  autorun() {
+    if (this.parent().subject()) {
+      const exams = Exams.find({category: this.parent().subject()}).fetch();
 
-                    return question;
-                })));
-                this.questions(questions);
-            } else {
-                this.questions([]);
-            }
-        }
-    },
+      if (exams.length) {
+        let questions = [];
+        _.each(exams, exam => questions = questions.concat(_.map(exam.questions, (question, index) => {
+          question.rawQuestion = question.question;
+          question.question = (index + 1) + ". " + question.question;
+          question.url = exam.url;
+          question.choices = _.map(question.choices, (choice, index) => choice = "(" + (index + 1) + ") " + choice);
+          question.exam = exam._id;
 
-    numVotes(question) {
-        return question.votes && question.votes.length || 0;
-    },
-
-    voteUp(q) {
-        console.log(q);
-        console.log("UP");
-        let index = -1, questions = Exams.findOne({_id: q.exam}).questions;
-        for (let i = 0; i < questions.length; i++) {
-            if (questions[i].question == q.rawQuestion) {
-                index = i;
-                break;
-            }
-        }
-
-        let update = {};
-        update[`questions.${index}.votes`] = Meteor.userId();
-        if (index != -1) {
-            if (_.find(questions, question => _.contains(question.votes, Meteor.userId()))) {
-                Exams.update({_id: q.exam}, {$pull: update});
-            } else {
-                Exams.update({_id: q.exam}, {$push: update});
-            }
-
-        }
-    },
-
-    voteDown() {
-        console.log('DOWN');
+          return question;
+        })));
+        this.questions(questions);
+      } else {
+        this.questions([]);
+      }
     }
+  },
+
+  numVotes(question) {
+    return question.votes && question.votes.length || 0;
+  },
+
+  voteUp(q) {
+    let index = -1, questions = Exams.findOne({_id: q.exam}).questions;
+    for (let i = 0; i < questions.length; i++) {
+      if (questions[i].question==q.rawQuestion) {
+        index = i;
+        break;
+      }
+    }
+
+    let update = {};
+    update[`questions.${index}.votes`] = Meteor.userId();
+
+    if (index!= -1) {
+      const query = questions[index];
+
+      if (query) {
+        if (_.find(query.votes, person => person===Meteor.userId())) {
+          Exams.update({_id: q.exam}, {$pull: update});
+        } else {
+          Exams.update({_id: q.exam}, {$push: update});
+        }
+      }
+    }
+  },
+
+  voteDown() {
+    console.log('DOWN');
+  }
 });
