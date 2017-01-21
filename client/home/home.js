68523a846d85a44036eca0989d0d84cc234cee43
Kenta Iwasaki <dranithix@gmail.com>
2017-01-21 15:53:00 -0500

    Working on upvotes/downvotes system.

--- client/home/home.js
+++ client/home/home.js
@@ -22,7 +22,6 @@ Template.home.viewmodel({
                 data: {
                     labels: ["Progress", "Todo"],
                     datasets: [
-
                         {
                             label: "Percentage",
                             data: [65, 35],
@@ -45,7 +44,7 @@ Template.home.viewmodel({
                 _.each(exams, exam => questions = questions.concat(_.map(exam.questions, question => {
                     question.url = exam.url;
                     question.choices = _.map(question.choices, (choice, index) => choice = (index + 1) + ") " + choice);
-
+                    question.exam = exam._id;
                     return question;
                 })));
                 this.questions(questions);
@@ -55,8 +54,31 @@ Template.home.viewmodel({
         }
     },
 
-    voteUp() {
+    numVotes(question) {
+        return question.votes && question.votes.length || 0;
+    },
+
+    voteUp(q) {
+        console.log(q.exam)
         console.log("UP")
+        let index = -1, questions = Exams.findOne({_id: q.exam}).questions;
+        for (let i = 0; i < questions.length; i++) {
+            if (questions[i].question == q.question) {
+                index = i;
+                break;
+            }
+        }
+
+        let update = {};
+        update[`questions.${index}.votes`] = Meteor.userId();
+        if (index != -1) {
+            if (_.find(questions, question => _.contains(question.votes, Meteor.userId()))) {
+                Exams.update({_id: q.exam}, {$pull: update});
+            } else {
+                Exams.update({_id: q.exam}, {$push: update});
+            }
+
+        }
     },
 
     voteDown() {
