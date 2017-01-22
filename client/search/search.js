05e1af19373602f20d72ceb7b9e4c753cee74195
Kenta Iwasaki <dranithix@gmail.com>
2017-01-22 03:48:46 -0500

    Working on search.

--- client/search/search.js
+++ client/search/search.js
@@ -2,8 +2,47 @@
  * Created by Kenta Iwasaki on 1/22/2017.
  */
 Template.search.viewmodel({
+    questions: null,
+    onCreated() {
+        Meteor.subscribe('exams.all');
 
-    autorun() {
-        console.log(this.parent().searchQuery());
+        const query = FlowRouter.getParam("query");
+        Meteor.call('question.search', query, (err, res) => {
+            if (!err) {
+                let questions = [];
+                let index = 1;
+                _.each(res, (exam, i) => {
+                    _.each(exam.questions, (question) => {
+                        question.question = (index++) + ". " + question.question;
+                        question.choices = _.map(question.choices, (choice, x) => "(" + (x + 1) + ") " + choice)
+                        question.tags = res[i].tags;
+                        question.exam = exam._id;
+                        questions.push(question)
+                    })
+                })
+
+                console.log(questions)
+                this.questions(questions);
+            }
+        })
     },
+    onRendered() {
+
+    },
+    numVotes(question) {
+        return question.votes && question.votes.length || 0;
+    },
+
+    gotoQuestion(question) {
+        console.log(question);
+
+        let index = -1, questions = Exams.findOne({_id: question.exam}).questions;
+        for (let i = 0; i < questions.length; i++) {
+            if (questions[i].question == question.question) {
+                index = i;
+                break;
+            }
+        }
+        FlowRouter.go(`/question/${question.exam}/${index}`);
+    }
 })
