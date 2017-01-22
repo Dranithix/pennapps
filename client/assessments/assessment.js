75a7d9ef32fc19a536d00f7a0d920fd7f06fffdd
helfi92 <helfi92@gmail.com>
2017-01-22 04:16:21 -0500

    Add Graphs on assessments

--- client/assessments/assessment.js
+++ client/assessments/assessment.js
@@ -1,3 +1,28 @@
-/**
- * Created by hassanali on 2017-01-22.
- */
+import Chart from "chart.js"
+
+Template.assessments.viewmodel({
+  onRendered() {
+    _.each($(".progressChart"), element => {
+      const power = parseInt(Math.random() * 100);
+      const progressChart = new Chart(
+        element,
+        {
+          type: 'pie',
+          data: {
+            labels: ["Progress", "To do"],
+            datasets: [
+              {
+                label: "Percentage",
+                data: [power, 100 - power],
+                backgroundColor: ["#6FE0B8"],
+                hoverBackgroundColor: ["#50C692"]
+              }
+            ]
+          },
+
+        }
+      )
+    })
+
+  },
+})
